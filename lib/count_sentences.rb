b8ce798e96de5a3bf64af6a5f75aef403d355de6
Nicholas Levenson <nicklevenson@gmail.com>
2020-10-28 09:39:56 -0700

    commit tree d4a36fce9e9d0f3caaac6129e8543e722a1431c1

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,23 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self[-1] == "."
+      true
+    else
+      false
+    end
   end
 
   def question?
-
+    self.end_with?("?") ? true : false
   end
 
   def exclamation?
-
+    self.end_with?("!") ? true : false
   end
 
   def count_sentences
-
+    self.split(/[.!?]+/).count
+   
   end
 end
